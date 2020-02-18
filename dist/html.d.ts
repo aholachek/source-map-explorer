e2b3dc2785a3b34e0cea3f15334fba0d33760055
Alex Holachek <aholachek@gmail.com>
2020-02-18 15:20:06 -0500

    commit dist

---
+++ dist/html.d.ts
@@ -0,0 +1,5 @@
+import { ExploreOptions, ExploreBundleResult } from './index';
+/**
+ * Generate HTML file content for specified files
+ */
+export declare function generateHtml(exploreResults: ExploreBundleResult[], options: ExploreOptions): string;
